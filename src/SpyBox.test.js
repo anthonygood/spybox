cad70b809c3e310fce0062f72b80faf85654c96d
Anthony Good <good.anthony@gmail.com>
2020-08-23 21:03:36 +0100

    tests for Spy export

--- src/SpyBox.test.js
+++ src/SpyBox.test.js
@@ -1,4 +1,5 @@
 const {
+  Spy,
   SpyBox,
   BottomlessBox,
   inspect,
@@ -176,5 +177,40 @@ describe('SpyBox', () => {
 
       expect(() => spy.a.b.c[2]).toThrowError()
     })
+  })
+})
+
+describe('Spy', () => {
+  it('by default works as simpler object stub', () => {
+    const spy = Spy()
+    spy.foo
+    spy.a
+    spy.a.b.c
+    spy.list.map
+
+    expect(inspect(spy)).toEqual({
+      a: {
+        b: {
+          c: {},
+        },
+      },
+      foo: {},
+      list: {
+        map: {}
+      }
+    })
+  })
+
+  it('can be named', () => {
+    const fooObject = Spy('fooObject')
+    const path = fooObject.a.b.c[1].d()[Symbol.toPrimitive]()
+
+    expect(path).toBe('Symbol.toPrimitive(fooObject.a.b.c.1.d())')
+  })
+
+  it('can set booby-traps on access paths', () => {
+    const root = Spy('root', { boobyTraps: ['root.a.b.c.2'] })
+
+    expect(() => root.a.b.c[2]).toThrowError()
   })
 })
